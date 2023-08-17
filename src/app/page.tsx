93ebea1a43125cb23e238115fe7749a46231c25d
Chris Seaman <christopherseaman1983@gmail.com>
2023-08-17 14:42:35 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
-export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">0</main>;
+export default async function Home() {
+  const res = await fetch("https://api.github.com/repos/CSEAMAN3/page-counter", { next: { revalidate: 5 } });
+  const data = await res.json();
+  console.log(data);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-[6rem]">{data.stargazers_count}</main>
+  );
 }
